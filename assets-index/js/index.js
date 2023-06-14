93989cafb86fda068f3aa80fffe4bcec5e8072cc
helionarfaria <helionar02faria@hotmail.com>
2023-06-14 20:07:19 -0300

    Padronização do header e footer em index; alteração na dinâmica entre index.html e index.js; transferência da classe 'main' de base.css para python.css

--- assets-index/js/index.js
+++ assets-index/js/index.js
@@ -1,15 +1,33 @@
 // ================================================= MAIN =================================================
-
 let texto1 = document.getElementById("texto1");
 let texto2 = document.getElementById("texto2");
 let texto3 = document.getElementById("texto3");
 let botao1 = document.getElementById("img1");
 let botao2 = document.getElementById("img2");
 let botao3 = document.getElementById("img3");
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let btnCalcular = document.getElementById("btnCalcular");
+let divinp1 = document.getElementById("divinp1");
+let divinp2 = document.getElementById("divinp2");
+let parabens = document.getElementById("parabens")
 
-botao1.addEventListener("click", () => {
-    texto1.classList.toggle('hidden')
+btnCalcular.addEventListener("click", ()=> {
+  if (input1.value * input2.value == 100) {
+  texto1.classList.toggle('hidden')
+  divinp1.remove();
+  divinp2.remove();
+  btnCalcular.remove();
+  parabens.innerText = "Muito bem, você acertou! Isto foi um exemplo bem básico do que podemos fazer com HTML e JavaScript.\nAgora pode continuar sua leitura!"
+  }
+  else {
+    alert("Valor incorreto, tente novamente!")
+  }
 })
+
+/*botao1.addEventListener("click", () => {
+    texto1.classList.toggle('hidden')
+})*/
 
 botao2.addEventListener("click", () => {
     texto2.classList.toggle('hidden')
@@ -18,57 +36,3 @@ botao2.addEventListener("click", () => {
 botao3.addEventListener("click", () => {
     texto3.classList.toggle('hidden')
 })
-
-
-// ================================================= FOOTER =================================================
-function togglePopup(input, label) {
-
-    // Mostrar Popup de Campo Obrigatório
-    input.addEventListener("focus",()=>{
-      label.classList.add("required-popup");
-    })
-    
-    // Ocultar Popup de Campo Obrigatório
-    input.addEventListener("blur",()=>{
-      label.classList.remove("required-popup");
-    })
-  }
-  
-  // ---------- VALIDAÇÃO EMAIL ---------- //
-  const emailInput = document.getElementById("email");
-  const emailHelper = document.getElementById("email-helper");
-  
-  togglePopup(emailInput)
-  
-  emailInput.addEventListener('input', (event) => {
-    const valorEmail = event.target.value;
-  
-    if (valorEmail.includes('@') && valorEmail.includes('.com')) {
-      emailInput.classList.remove('error');
-      emailInput.classList.add('correct');
-      emailHelper.classList.remove('visible');
-      checkInputs.emailInput = true;
-    } else {
-      emailInput.classList.remove('correct');
-      emailInput.classList.add('error');
-      emailHelper.innerText = 'O Email Precisa Incuir um "@" e um ".com"';
-      emailHelper.classList.add('visible');
-      checkInputs.emailInput = false;
-    }
-  })
-  
-  // ---------- ENVIAR FORMULÁRIO ---------- //
-  const btnSubmit = document.querySelector("button[type ='submit']")
-  
-  const checkInputs = {
-    emailInput: false,
-  }
-  
-  btnSubmit.addEventListener("click", (e) =>{
-    if (checkInputs.emailInput == false) {
-      e.preventDefault()
-      alert('O Email Precisa Ser Preenchido Corretamente')
-    } else {
-      alert("Você foi Inscrito com Sucesso!")
-    }
-  })
