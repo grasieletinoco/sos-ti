8208c774c4889b7af5b3791e458c464dd6eec88c
grasiele <grasieleprt@gmail.com>
2023-06-21 01:45:44 -0300

    ADD Button Back To Top Página Python

--- assets-base/js/base.js
+++ assets-base/js/base.js
@@ -85,3 +85,28 @@ function correctSubmitModal(){
 }
 
 //FIM NEWSLETTER
+
+
+//INICIO BUTTON BACK TO TOP
+var btnTop = document.querySelector('#button_top');
+
+  var btnReveal = function () { 
+    if (window.scrollY > 300) {
+      btnTop.classList.add('visible');
+    } else {
+      btnTop.classList.remove('visible');
+    }    
+  }  
+
+  var TopscrollTo = function () {
+    if(window.scrollY!=0) {
+      setTimeout(function() {
+        window.scrollTo(0,window.scrollY-30);
+        TopscrollTo();
+      }, 5);
+    }
+  }
+  window.addEventListener('scroll', btnReveal);
+  btnTop.addEventListener('click', TopscrollTo);
+
+  //FINAL BUTTON BACK TO TOP
